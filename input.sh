cdc3f29f24346597f28e62459e58f45311bb65ab
Ishani <ipandiya12@gmail.com>
2026-06-27 17:43:13 +0530

    read from prompt:first draft

---
+++ input.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+#This will showcase how to read input from prompt
+
+#basics: use "read" command
+
+echo "what species are you?"
+read species
+echo "Got it! you are a $species "
+
+
+# To read with prompt use -p flag
+
+read -p "Enter your username:" username
+
+
+# For security on input like password, use flag -s
+
+read -sp "Enter your password:" pass
+
+Echo "your creds are: $username , $pass"
+
+
+
+
+
