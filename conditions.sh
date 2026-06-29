27e3f73155b68ce3648b83f392236ae5d8b3f448
Ishani <ipandiya12@gmail.com>
2026-06-29 23:59:06 +0530

    first draft

---
+++ conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+read -p "enter your age" age
+
+if [ $age -gt 30 ]; then
+echo "youre old officially"
+
+elif [ $age -lt 20 ]; then
+echo "youre a babyy"
+
+else
+echo "youre either traumatised or anxious"
+
+fi
+
+
